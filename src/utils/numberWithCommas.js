af073508dbc7674c4923bda9cc80ba7fc02e62c4
artemats <artem.matseruk@gmail.com>
2021-04-08 10:46:06 +0300

    replace number reges

--- src/utils/numberWithCommas.js
+++ src/utils/numberWithCommas.js
@@ -1,4 +1,4 @@
 export const numberWithCommas = (x) => {
     // return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-    return x;
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
