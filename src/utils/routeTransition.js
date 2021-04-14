87e7edcaed407c7c0814ca5fde79a66da90de4d5
artemats <artem.matseruk@gmail.com>
2021-04-14 10:27:13 +0300

    add scroll update on start

--- src/utils/routeTransition.js
+++ src/utils/routeTransition.js
@@ -62,6 +62,7 @@ export const routeIn = (pathname, node, appears) => {
                 }
                 scaleIn();
                 fadeInContent();
+                locoScroll.update();
                 //end display split title
             }
         }
