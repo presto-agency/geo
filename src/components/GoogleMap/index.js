b4bde56a65896f081b2088492b691ba89ebf6767
artemats <artem.matseruk@gmail.com>
2021-05-06 14:38:53 +0300

    setup test API key map

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -11,7 +11,7 @@ const map = {
         gridSize: 50
     },
 };
-const KEY =  process.env.REACT_APP_GOOGLE_API_KEY;
+const KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const GoogleMap = ({ data }) => {
 
